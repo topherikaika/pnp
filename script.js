070892de401b265fadc2fa79a535c9770aa7b4b9
Christopher Ikaika Crespo <topherikaika@gmail.com>
2024-01-03 15:42:06 +0900

    create mediaStream variable in select<ediaStream function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const button = document.getElementById("button");
 
 async function selectMediaStream() {
   try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
   } catch (error) {}
 }
 
