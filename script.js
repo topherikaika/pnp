19f3e33dd4d473eee4be04c425f6c3c3c9b8ed7a
Christopher Ikaika Crespo <topherikaika@gmail.com>
2024-01-03 15:39:27 +0900

    create async function selectMediaStream with try and catch block, call function afterwards

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const videoElement = document.getElementById("video");
 const button = document.getElementById("button");
+
+// Prompt user to select a media stream, pass to videoElement, then play
+
+async function selectMediaStream() {
+  try {
+  } catch (error) {}
+}
+
+// On load
+selectMediaStream();
