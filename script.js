75fe14ec05821fc3fbdbb11a02a312b71b1ebca8
Christopher Ikaika Crespo <topherikaika@gmail.com>
2024-01-03 15:53:25 +0900

    pass media Stream to videoelement as soruce object, when video meta data loaded call video function and create error message

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const button = document.getElementById("button");
 async function selectMediaStream() {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-  } catch (error) {}
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("whooops, something went wrong...", error);
+  }
 }
 
 // On load
